a3037827fcb94fe61f6256dd5d30d2f6084719e7
newbz521 <lawrence.yee1991@gmail.com>
2022-05-20 19:04:46 -0400

    media query + component changes

--- haterfrontend/src/components/profilebutton/Profilebutton.js
+++ haterfrontend/src/components/profilebutton/Profilebutton.js
@@ -1,8 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import "./Profilebutton.css";
+import MENU from "../profilebutton/icons8-menu-50.png";
+import GIF from "../profilebutton/menu.gif";
 
 function Probutton(props) {
-  return <div className="probutton" onClick={props.click}></div>;
+  const [slideIn, setSlideIn] = useState({ display: "flex" });
+  const [menu, setMenu] = useState(MENU);
+  const [toggle, setToggle] = useState(true);
+
+  function setMenuDisplay() {
+    console.log("changed");
+    if (toggle == true) {
+      props.setSlideIn({ transform: "translate(0%, 0%)" });
+      setMenu(GIF);
+    } else {
+      props.setSlideIn({ transform: "translate(-1000px, 0%)" });
+      setMenu(MENU);
+    }
+    setToggle((prevCheck) => !prevCheck);
+  }
+
+  return (
+    <div className="proButtonBack">
+      <img className="probutton" onClick={setMenuDisplay} src={menu}></img>
+    </div>
+  );
 }
 
 export default Probutton;
