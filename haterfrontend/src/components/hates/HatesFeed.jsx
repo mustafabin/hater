a006cdab730f0ff0dd20721d879f4c1f4e9b3fed
mustafabin <mbin828@gmail.com>
2022-05-21 11:44:35 -0400

    added a loading screen for hates feed

--- haterfrontend/src/components/hates/HatesFeed.jsx
+++ haterfrontend/src/components/hates/HatesFeed.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Hate from "./Hate";
+import gifSrc from "../../images/loading.gif";
 
 export default function HatesFeed({ hates, setToggle, pageNum }) {
   const [hatesPaged, setHatesPaged] = useState([]);
+  const [loaded, setLoaded] = useState(false);
   useEffect(() => {
     setHatesPaged(hates.slice(pageNum[0], pageNum[1]));
   }, [hates, pageNum]);
-
+  useEffect(() => {
+    setLoaded(true);
+  }, [hatesPaged]);
+  let loadingGif = (
+    <img src={gifSrc} alt="Loading screen" className="loading-gif" />
+  );
   return (
     <div className="hates-list">
-      {hatesPaged.map((hate, id) => (
-        <Hate setToggle={setToggle} key={id} hateData={hate} />
-      ))}
+      {!loaded
+        ? loadingGif
+        : hatesPaged.map((hate, id) => (
+            <Hate setToggle={setToggle} key={id} hateData={hate} />
+          ))}
     </div>
   );
 }
